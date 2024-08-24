b803ae4c266296d511d3cbe613d4df22fe9a52fc
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-08-24 12:33:11 +0200

    add: Pizza class attributes and relatives getters and setters

--- src/main/java/org/lesson/java/spring/model/Pizzas.java
+++ src/main/java/org/lesson/java/spring/model/Pizzas.java
@@ -1,5 +1,72 @@
 package org.lesson.java.spring.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pizzas")
 public class Pizzas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	private String descrizione;
+	
+	@Column(name="foto_link")
+	private String img_url;
+	
+	private double prezzo;
+	
+	//Getters and Setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getImg_url() {
+		return img_url;
+	}
+
+	public void setImg_url(String img_url) {
+		this.img_url = img_url;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	
+	
+	
 
 }
