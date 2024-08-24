6a5d54cf86f2e930d793314f881a15b30216001c
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-08-24 12:39:59 +0200

    add: PizzaRepository interface

---
+++ src/main/java/org/lesson/java/spring/controller/PizzaRepository.java
@@ -0,0 +1,8 @@
+package org.lesson.java.spring.controller;
+
+import org.lesson.java.spring.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer>{
+
+}
