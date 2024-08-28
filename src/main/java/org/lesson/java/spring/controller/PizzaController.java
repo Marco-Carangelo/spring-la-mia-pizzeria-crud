13f73902c7ddbe56ffabd6db5d4e3e430dce2445
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-08-28 15:07:39 +0200

    add: show method to map the view of a single object

--- src/main/java/org/lesson/java/spring/controller/PizzaController.java
+++ src/main/java/org/lesson/java/spring/controller/PizzaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +22,12 @@ public class PizzaController {
 	List<Pizza> result = repository.findAll();
 	model.addAttribute("list", result);
 	return "/pizzas/index";
+	}
+	
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable("id") Integer id , Model model) {
+	model.addAttribute("pizza", repository.findById(id).get());
+	return "/pizzas/show";
 	}
 	
 
