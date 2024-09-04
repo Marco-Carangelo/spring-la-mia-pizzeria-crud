fe813a39be262b7c5cd5b9666159a1e3845bd915
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-04 20:06:33 +0200

    add: post method for the route "/create"

--- src/main/java/org/lesson/java/spring/controller/PizzaController.java
+++ src/main/java/org/lesson/java/spring/controller/PizzaController.java
@@ -6,8 +6,11 @@ import org.lesson.java.spring.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -43,5 +46,20 @@ public class PizzaController {
 		model.addAttribute("pizza", new Pizza());
 		return "/pizzas/create";
 	}
+	
+	@PostMapping("/create")
+	public String store(@ModelAttribute("pizza") Pizza formPizza,
+			BindingResult bindingResult,
+			Model model){
+		
+		if(bindingResult.hasErrors()) {
+			return "/pizzas/create";
+		}
+		
+		repository.save(formPizza);
+		
+		return "redirect:/pizas";
+	}
+	
 
 }
