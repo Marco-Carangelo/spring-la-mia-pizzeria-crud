b66f3070320318c7885ac0087a98b17a7658814c
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-05 17:05:52 +0200

    update: validation annotation in the the Pizza class

--- src/main/java/org/lesson/java/spring/model/Pizza.java
+++ src/main/java/org/lesson/java/spring/model/Pizza.java
@@ -2,12 +2,19 @@ package org.lesson.java.spring.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "pizzas")
@@ -17,17 +24,26 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotNull
+	@NotEmpty
 	private String nome;
 	
+	@NotEmpty
+	@NotNull
 	private String descrizione;
 	
 	@Column(nullable = true)
 	private String img_url;
 	
+	@NotNull
+	@Min(3)
+	@Max(50)
 	private double prezzo;
 	
+	@UpdateTimestamp
 	private LocalDateTime createdAt;
 	
+	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 	
 
