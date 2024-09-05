3592c20b09dd8464e81c22bcbbcf9f64b82d2c5b
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-05 17:42:38 +0200

    update: deleted useless import

--- src/main/java/org/lesson/java/spring/model/Pizza.java
+++ src/main/java/org/lesson/java/spring/model/Pizza.java
@@ -14,7 +14,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "pizzas")
